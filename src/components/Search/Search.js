ffa5da0155f650e141786b284f16e62ab423d0f5
langwenda <langwdalan@gmail.com>
2019-01-15 17:48:20 +0800

    加入图标

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
+import { Icon } from 'antd';
 import './Search.scss';
 export default class Search extends Component {
   render() {
     const {timeout,data} = this.props;
     let result = ''
+    let type = ''
     if(data.length > 0){
       result = data[0].title;
+      type = data[0].value.startsWith('http://') || data[0].value.startsWith('https://')?'global':'desktop'
     }
     return (
       <div className='search-container'>
         <div className='mainbar'>
+        {type?<Icon type={type} className="icon" />:null}
           {result}
         </div>
         <div className={classNames('keycontainer',{'underline':!timeout})}>
